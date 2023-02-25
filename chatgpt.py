bcc4240ba16ee7d53cd34009d64cd363894ad545
Sahil <77140242+SahilPadhiyar18@users.noreply.github.com>
2023-02-25 15:54:27 +0530

    Initial commit

---
+++ chatgpt.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request
+import openai
+
+
+app = Flask(__name__)
+openai.api_key = "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"
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/chat", methods=["POST"])
+def chat():
+    user_input = request.form["user_input"]
+    bot_response = generate_text(user_input)
+    return {"response": bot_response}
+
+
+def generate_text(prompt):
+    completions = openai.Completion.create(
+        engine="text-davinci-003", # You can choose a different GPT model here
+        prompt=prompt,
+        max_tokens=12,
+        n=1,
+        stop=None,
+        temperature=0.5,
+    )
+
+    message = completions.choices[0].text.strip()
+    return message
+
+if __name__ == "__main__":
+    app.run(debug=True)
